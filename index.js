cbbfeb232cd563b5f34faef1a94f18f42486bd80
Gideon Mutai <giddymutai13@gmail.com>
2024-04-05 14:54:48 +0300

    Overviews for loops

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// A function that iterated over the names list and outputs an array of thank you messages
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(array) {
+  let msgs = [];
+  for (let i = 0; i < array.length; i++) {
+    let msg = `Thank you, ${array[i]}, for the wonderful surprise gift!`;
+    msgs.push(msg);
+  }
+  return msgs;
+}
+
+writeCards(names);
+
+// A function that counts down the numbers upto the number provided
+function countDown(num) {
+  for (let i = 0; i < num+1; i++) {
+    console.log(i);
+  }
+}
+
+countDown(10)
